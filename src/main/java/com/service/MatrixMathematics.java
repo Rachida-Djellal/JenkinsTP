4ded48497cd46a1d7d446122172f369901388fec
Asmaalagrid <42660545+LagridAs@users.noreply.github.com>
2020-02-26 02:13:26 +0100

    Update MatrixMathematics.java

--- src/main/java/com/service/MatrixMathematics.java
+++ src/main/java/com/service/MatrixMathematics.java
@@ -11,13 +11,7 @@ public class MatrixMathematics {
 	private MatrixMathematics(){}
 	
 	
-	/**
-	 * Determinant of a square matrix
-	 * The following function find the determinant in a recursively. 
-	 * @param matrix
-	 * @return
-	 * @throws NoSquareException
-	 */
+	
 	public static double determinant(Matrix matrix) throws NoSquareException {
 		if (!matrix.isSquare())
 			throw new NoSquareException("matrix need to be square.");
